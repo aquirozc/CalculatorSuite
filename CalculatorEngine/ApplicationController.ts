516012776790082bb743c69e065c445c0d83833c
Alejandro Quiroz Carmona <alejandroqc03@outlook.com>
2023-03-30 09:16:01 -0600

    Added grammar checks

--- CalculatorEngine/ApplicationController.ts
+++ CalculatorEngine/ApplicationController.ts
@@ -51,39 +51,63 @@ function bloquearOpcion(opcionSeleccionada : string){
 
 }
 
+function comprobarGramatica(valorEntrada : number | string, caracteresAdmitidos : string [] ){
+
+    let cumpleConLaGramatica : boolean = true;
+
+    for(let digito of valorEntrada.toString()){
+
+        if(!caracteresAdmitidos.includes(digito)){
+
+            cumpleConLaGramatica = false;
+            break;
+
+        }
+
+    }
+
+    if(!cumpleConLaGramatica){
+        
+        alert("Los valores introducidos no son validos.");
+        throw new Error();
+
+    }
+
+}
+
 function convertirEntrada(){
 
     let entradaUsuario: HTMLInputElement = document.getElementById('prompt-textbox')! as HTMLInputElement;
-    
     let formatoFuente : HTMLInputElement = document.querySelector('input[name="sourceformat"]:checked')!;
     let formatoSalida : HTMLInputElement = document.querySelector('input[name="outcomeformat"]:checked')!;
-
+    let valorEntrada : string = entradaUsuario.value.replace(/\s/g,'');;
     let valorSalida : string = '';
-
     let etiquetaFuente : string = formatoFuente.value.substring(formatoFuente.value.length - 2);
-    let etiquetaSalida : string = formatoSalida.value.substring(formatoSalida.value.length - 2);;
+    let etiquetaSalida : string = formatoSalida.value.substring(formatoSalida.value.length - 2);
 
     switch(formatoFuente.value){
 
         case 'base02' : 
 
+            comprobarGramatica(valorEntrada,['0','1']);
+
             switch(formatoSalida.value){
 
                 case 'base08' :
 
-                    valorSalida = <string> <unknown> analizadorEntrada.convertirNumeroBinarioAOctal(entradaUsuario.value);
+                    valorSalida = <string> <unknown> analizadorEntrada.convertirNumeroBinarioAOctal(valorEntrada);
 
                 break;
 
                 case 'base10' :
 
-                    valorSalida = <string><unknown>analizadorEntrada.convertirNumeroBinarioADecimal(entradaUsuario.value);
+                    valorSalida = <string><unknown>analizadorEntrada.convertirNumeroBinarioADecimal(valorEntrada);
 
                 break;
 
                 case 'base16' :
 
-                    valorSalida = analizadorEntrada.convertirNumeroBinarioAHexadecimal(entradaUsuario.value);
+                    valorSalida = analizadorEntrada.convertirNumeroBinarioAHexadecimal(valorEntrada);
 
                 break;
 
@@ -93,23 +117,25 @@ function convertirEntrada(){
 
         case 'base08' : 
 
+            comprobarGramatica(valorEntrada,['0','1','2','3','4','5','6','7']);
+
             switch(formatoSalida.value){
 
                 case 'base02' :
 
-                    valorSalida = analizadorEntrada.convertirNumeroOctalABinario(<number><unknown> entradaUsuario.value);
+                    valorSalida = analizadorEntrada.convertirNumeroOctalABinario(<number><unknown> valorEntrada);
 
                 break;
 
                 case 'base10' :
 
-                    valorSalida = <string><unknown> analizadorEntrada.convertirNumeroOctalADecimal(<number><unknown> entradaUsuario.value);
+                    valorSalida = <string><unknown> analizadorEntrada.convertirNumeroOctalADecimal(<number><unknown> valorEntrada);
 
                 break;
 
                 case 'base16' :
 
-                    valorSalida = analizadorEntrada.convertirNumeroOctalAHexadecimal(<number><unknown> entradaUsuario.value);
+                    valorSalida = analizadorEntrada.convertirNumeroOctalAHexadecimal(<number><unknown> valorEntrada);
 
                 break;
 
@@ -119,23 +145,25 @@ function convertirEntrada(){
 
         case 'base10' : 
 
+            comprobarGramatica(valorEntrada,['0','1','2','3','4','5','6','7','8','9']);
+
             switch(formatoSalida.value){
 
                 case 'base02' :
 
-                    valorSalida = analizadorEntrada.convertirNumeroDecimalABinario(<number> <unknown> entradaUsuario.value);
+                    valorSalida = analizadorEntrada.convertirNumeroDecimalABinario(<number> <unknown> valorEntrada);
 
                 break;
 
                 case 'base08' :
 
-                    valorSalida = <string><unknown>analizadorEntrada.convertirNumeroDecimalAOctal(<number><unknown> entradaUsuario.value);
+                    valorSalida = <string><unknown>analizadorEntrada.convertirNumeroDecimalAOctal(<number><unknown> valorEntrada);
 
                 break;
 
                 case 'base16' :
 
-                    valorSalida = analizadorEntrada.convertirNumeroDecimalAHexadecimal(<number><unknown> entradaUsuario.value);
+                    valorSalida = analizadorEntrada.convertirNumeroDecimalAHexadecimal(<number><unknown> valorEntrada);
 
                 break;
 
@@ -145,23 +173,25 @@ function convertirEntrada(){
 
         case 'base16' : 
 
+            comprobarGramatica(valorEntrada,['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']);
+
             switch(formatoSalida.value){
 
                 case 'base02' :
 
-                    valorSalida = analizadorEntrada.convertirNumeroHexadecimalABinario(entradaUsuario.value);
+                    valorSalida = analizadorEntrada.convertirNumeroHexadecimalABinario(valorEntrada);
 
                 break;
 
                 case 'base08' :
 
-                    valorSalida = <string><unknown> analizadorEntrada.convertirNumeroHexadecimalAOctal(entradaUsuario.value);
+                    valorSalida = <string><unknown> analizadorEntrada.convertirNumeroHexadecimalAOctal(valorEntrada);
 
                 break;
 
                 case 'base10' :
 
-                    valorSalida = <string><unknown> analizadorEntrada.convertirNumeroHexadecimalADecimal(entradaUsuario.value);
+                    valorSalida = <string><unknown> analizadorEntrada.convertirNumeroHexadecimalADecimal(valorEntrada);
 
                 break;
 
@@ -171,7 +201,7 @@ function convertirEntrada(){
 
     }
 
-    document.getElementById('source-value')!.innerText = entradaUsuario.value ;
+    document.getElementById('source-value')!.innerText = entradaUsuario.value;
     document.getElementById('source-base')!.innerHTML = '<span>Base <br><span class="highlight-2">'+etiquetaFuente+'</span></span>';
     document.getElementById('outcome-value')!.innerText = valorSalida ;
     document.getElementById('outcome-base')!.innerHTML = '<span>Base <br><span class="highlight-2">'+etiquetaSalida+'</span></span>';
